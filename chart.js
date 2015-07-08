9f75376c02f160169f98a76e435342a6b59b6f65
DishankTiwari <dishank@fusioncharts.com>
2015-07-08 12:00:41 +0530

    Edited

--- chart.js
+++ chart.js
@@ -1,39 +1,10 @@
-var chart = {
-	width : 500,
-	height : 500,
-		list : [
-		{
-			"label":"Jan",
-			"value":"500"
-		},
-		{
-			"label":"Feb",
-			"value":"200"
-		},
-		{
-			"label":"March",
-			"value":"400"
-		},
-		{
-			"label":"April",
-			"value":"150"
-		},
-		{
-			"label":"May",
-			"value":"300"
-		},
-		{
-			"label":"June",
-			"value":"400"
-		}
-		]
-	};
 window.onload = function () {
 	var paper = Raphael("container", 1000, 1000);
 	var x = 100;
 	var y = 550;
 	var arr = []; 
-	var rect = paper.rect(100, 50, (chart.width)+20, chart.height)
+	var c1 = null, c2 = null;
+	rectangle : var rect = paper.rect(100, 50, (chart.width)+20, chart.height)
 	.attr("stroke", "red")
 	.attr("stroke-width", "3")
 	//debugger;
@@ -43,13 +14,12 @@ window.onload = function () {
 			for(var j = 0; j < chart.list.length; j++){
 				arr[j] = chart.list[j].value;
 			}	
-
 		}
 	}
 	var max = 0;
 	for (var j = 0; j < arr.length; j++) {
-	if(arr[j] > max)
-	max = arr[j];
+		if(arr[j] > max)
+			max = arr[j];
 	}
 	//console.log(max);
 	if(max >= chart.height){
@@ -61,6 +31,7 @@ window.onload = function () {
 	}
 	function line(startX,startY,endX,endY){
 		return paper.path("M"+startX+" "+startY+"L"+endX+" "+endY)
+		.attr("stroke", "lightred")
 		.attr("stroke-dasharray", "-.").toBack();
 	}
 	var a = x,r;
@@ -68,26 +39,42 @@ window.onload = function () {
 	for(var i = 0; i < arr.length; i++){
 		a = a + 20;
 		paper.rect(a, y-arr[i], r, arr[i])
-		.attr("fill", "lightblue")
-		//.attr("title", "chart.list[i].value");
+		.attr("fill", "skyblue")
+		.mouseover(function() {
+            this.attr("fill", "yellow")
+           // alert(400)
+        })
+        .mouseout(function() {
+            this.attr("fill", "skyblue")
+        });
 		a = a + r;
+		/*c1.mouseover(function(){
+        	alert();
+		});
+c1.node.onclick = function () {
+    alert(chart.list[i].value);
+};*/
 		paper.text(a-30, y-arr[i]+10, chart.list[i].value)
 		.attr("font-size", "15")
-		.attr("fill", "blue");
-		paper.text(a-30, y+10, chart.list[i].label)
+		.attr("fill", "blue")
+		.mouseover(function(){
+			this.attr("fill", "red")
+			this.alert(chart.list[i].value)
+		});
+		label : paper.text(a-30, y+10, chart.list[i].label)
 		.attr("fill", "brown")
 		.attr("font-size", "15");
 		//paper.text(x, chart.height-arr.length-j, );
-		for(j = 0; j < 450; j+=50){
-			line(x, chart.height-arr.length-j, chart.width+120, chart.height-arr.length-j);
+		for(j = 0; j < 500; j+=75){
+			line(x, y-j, chart.width+120, y-j);
 		}
-		paper.text(chart.width-140, chart.height-470, "Column Chart")
+		captionName : paper.text(chart.width-140, chart.height-470, "Column Chart")
 		 .attr("font-size", "50")
 		 .attr("fill", "darkBlue");
-		paper.text(chart.width-140, chart.height+90, "Monthly Division")
+		yAxisName : paper.text(chart.width-140, chart.height+90, "Monthly Division")
 		 .attr("font-size", "20")
 		 .attr("fill", "darkRed");
-		paper.text(chart.width-450, chart.height-200, "Electric Expences")
+		xAxisName : paper.text(chart.width-450, chart.height-200, "Electric Expences")
 		 .rotate(-90,[chart.width-450],[chart.height-200])
 		 .attr("font-size", "20")
 		 .attr("fill", "darkRed");
